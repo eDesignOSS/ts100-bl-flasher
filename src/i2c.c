ea84e413b238b3684aad9fe69f2f74ecd5aa5db0
perillamint <perillamint@gentoo.moe>
2017-09-17 22:27:35 +0900

    I2C driver

---
+++ src/i2c.c
@@ -0,0 +1,37 @@
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/i2c.h>
+#include <libopencm3/stm32/rcc.h>
+
+#include "i2c.h"
+
+void i2c_init(void) {
+	rcc_periph_clock_enable(RCC_GPIOB | RCC_I2C1);
+  gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+                GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
+                GPIO_I2C2_SCL | GPIO_I2C2_SDA);
+  i2c_peripheral_disable(I2C1);
+  i2c_set_clock_frequency(I2C1, I2C_CR2_FREQ_36MHZ);
+  i2c_set_fast_mode(I2C1);
+  i2c_set_ccr(I2C1, 0x1e);
+  i2c_set_trise(I2C1, 0x0b);
+  i2c_peripheral_enable(I2C1);
+}
+
+void i2c_write(uint8_t *data, uint32_t size, uint8_t addr) {
+  i2c_send_start(I2C1);
+
+  // Wait for I2C start pkt
+  while (!((I2C_SR1(I2C1) & I2C_SR1_SB)
+           & (I2C_SR2(I2C1) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
+
+  i2c_send_7bit_address(I2C1, addr, I2C_WRITE);
+  while (!(I2C_SR1(I2C1) & I2C_SR1_ADDR));
+
+  for(uint32_t i = 0; i < size; i++) {
+    i2c_send_data(I2C1, data[i]);
+    while (!(I2C_SR1(I2C1) & I2C_SR1_BTF));
+  }
+
+  while (!(I2C_SR1(I2C1) & I2C_SR1_TxE));
+  i2c_send_stop(I2C1);
+}
---
+++ src/i2c.h
@@ -0,0 +1,2 @@
+void i2c_init(void);
+void i2c_write(uint8_t *data, uint32_t size, uint8_t addr);
