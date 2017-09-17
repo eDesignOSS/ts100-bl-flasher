fae26245ec0e1ebae2eb5505e07d324b856224d4
perillamint <perillamint@gentoo.moe>
2017-09-17 22:27:25 +0900

    systick driver

---
+++ src/systick.c
@@ -0,0 +1,23 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/cm3/systick.h>
+
+#include "systick.h"
+
+uint32_t count = 0;
+
+void sys_tick_handler(void) {
+  count ++;
+}
+
+void systick_init(uint64_t freq) {
+  systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
+  systick_set_reload(freq / 8 / 1000 - 1);
+  systick_interrupt_enable();
+  systick_counter_enable();
+}
+
+void delay_ms(uint32_t ticks) {
+  uint32_t endtime = ticks + count;
+
+  while(count < endtime);
+}
---
+++ src/systick.h
@@ -0,0 +1,3 @@
+void sys_tick_handler(void);
+void systick_init(uint64_t freq);
+void delay_ms(uint32_t ticks);
